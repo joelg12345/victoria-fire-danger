525d800f75c9e440fb621ac245849028c5bd170c
joelg12345 <joel.gilmour@hotmail.com>
2026-02-02 21:39:06 +1100

    Update vic-fire-danger-card.js

--- custom_components/victoria_fire_danger/www/vic-fire-danger-card.js
+++ custom_components/victoria_fire_danger/www/vic-fire-danger-card.js
@@ -191,7 +191,7 @@ if (!customElements.get('vic-fire-danger-card')) {
 }
 
 console.info(
-  '%c VIC-FIRE-DANGER-CARD %c Version 1.0.6 ',
+  '%c VIC-FIRE-DANGER-CARD %c Version 1.0.7 ',
   'color: white; background: #ce161e; font-weight: 700;',
   'color: #ce161e; background: white; font-weight: 700;'
 );
